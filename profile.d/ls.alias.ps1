6bda67621efff93fd0844eafbe540f50a3f9d535
sachabruttin <sacha@bruttin.com>
2019-04-03 08:57:49 +0200

    Changes

--- profile.d/ls.alias.ps1
+++ profile.d/ls.alias.ps1
@@ -1,2 +1,4 @@
 # ls
 Set-Alias ls Get-ChildItem-Color -option AllScope -Force
+# dir
+Set-Alias dir Get-ChildItem-Color -Option AllScope -Force
--- profile.d/visual-studio.ps1
+++ profile.d/visual-studio.ps1
@@ -1,9 +1,9 @@
 ## Visual Studio Command Prompt support
-Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools"
-cmd /c "VsDevCmd.bat&set" |
-ForEach-Object {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
-}
-Pop-Location
+##Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools"
+##cmd /c "VsDevCmd.bat&set" |
+##ForEach-Object {
+##  if ($_ -match "=") {
+##    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+##  }
+##}
+##Pop-Location
